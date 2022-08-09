6715395510f0c39a67c7648378b0327cf58887f2
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-08-09 16:26:09 +0300

    basic render map

--- src/index.js
+++ src/index.js
@@ -1,42 +1,59 @@
-// const axios = require('axios').default;
 import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-// const instance = axios.create({
-//   baseURL: 'https://pixabay.com/api/',
-//   headers: { common: { Authorization: '29101880-694af7e9974b3c9bb9fbf3052' } },
-// });
 const searchForm = document.querySelector('#search-form');
-const searchParams = new URLSearchParams({
-  image_typemit: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-});
+const gallery = document.querySelector('.gallery');
 searchForm.addEventListener('input',  event => {
   const inputValue = event.currentTarget.elements.searchQuery.value;
     axios
-      .get(
-        // `https://pixabay.com/api/?key=29101880-694af7e9974b3c9bb9fbf3052&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`
-        // `${instance.defaults.baseURL}?key=${instance.defaults.headers.common['Authorization']}&q=${inputValue}&${searchParams}`
-        // `https://pixabay.com/api/?q=${inputValue}&${searchParams}`,
-        `?q=${inputValue}`,
-        {
-          params: {
-            key: '29101880-694af7e9974b3c9bb9fbf3052',
-            image_typemit: 'photo',
-            orientation: 'horizontal',
-            safesearch: true,
-          },
-        }
-      )
+      .get(`?q=${inputValue}`, {
+        params: {
+          key: '29101880-694af7e9974b3c9bb9fbf3052',
+          image_typemit: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+        },
+      })
       .then(function (response) {
         // handle success
-        console.log(response);
+        response.data.total === 0 &&
+          Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        return response;
       })
       .catch(function (error) {
         // handle error
         console.log(error);
       })
-      .then(function () {
+      .then(function (response) {
         // always executed
+        console.log(response.data.hits);
+        gallery.insertAdjacentHTML(
+          'afterbegin',
+          response.data.hits
+            .map(
+              element => `
+<div class="photo-card">
+  <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b> ${element.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b> ${element.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b> ${element.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b> ${element.downloads}
+    </p>
+  </div>
+</div>
+`
+            )
+            .join('')
+        );
       });
 });
